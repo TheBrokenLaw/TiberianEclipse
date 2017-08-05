f6e43ef3a510c613149d01c0e04b149a44c55f92
TheBrokenLaw <lordoflard007@gmail.com>
2017-08-05 17:12:38 -0400

    Fixed worldgen for real this time

--- src/main/java/tiberianeclipse/Main.java
+++ src/main/java/tiberianeclipse/Main.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import tiberianeclipse.block.ModBlocks;
 import tiberianeclipse.item.ModItems;
 import net.minecraftforge.fml.common.Mod;
@@ -14,13 +15,14 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import tiberianeclipse.recipes.ModRecipes;
 import tiberianeclipse.util.EclipseTab;
+import tiberianeclipse.world.ModWorldGen;
 
 @Mod(modid = Main.modId, name = Main.name, version = Main.version, acceptedMinecraftVersions = "[1.10.2]")
 public class Main {
 
     public static final String modId = "TiberianEclipse";
     public static final String name = "Tiberian Eclipse";
-    public static final String version = "1.10.2-0.01b";
+    public static final String version = "1.10.2-0.02b";
     @Mod.Instance(modId)
     public static Main instance;
     @SidedProxy(serverSide = "tiberianeclipse.CommonProxy", clientSide = "tiberianeclipse.ClientProxy")
@@ -34,9 +36,8 @@ public class Main {
     public void preInit(FMLPreInitializationEvent event) {
         ModBlocks.init();
         ModItems.init();
-
+        GameRegistry.registerWorldGenerator(new ModWorldGen(), 3);
     }
-
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
