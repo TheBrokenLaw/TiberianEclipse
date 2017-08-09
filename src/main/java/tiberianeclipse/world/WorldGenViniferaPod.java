bf975ddd51f407f9d92a8e4a43ed89317b0fb304
TheBrokenLaw <lordoflard007@gmail.com>
2017-08-09 12:36:09 -0400

    Changed the max age that the various pods generate at

--- src/main/java/tiberianeclipse/world/WorldGenViniferaPod.java
+++ src/main/java/tiberianeclipse/world/WorldGenViniferaPod.java
@@ -46,49 +46,7 @@ public class WorldGenViniferaPod extends WorldGenerator {
 
     int randomAge(Random random)
     {
-        return random.nextInt(7);
+        return random.nextInt(5);
     }
-   // public boolean generate(World worldIn, Random rand, BlockPos pos)
-   // {
 
-// we randomly pick between a bush with a cookie and a bush without a cookie
-
-   //     Block block = ModBlocks.viniferaPod;
-   //     int y = 1 + getGroundFromAbove(worldIn, pos.getX(), pos.getZ());
-        // debug:
-        // System.out.println("Y-value of ground is " + y + " at (" + pos.getX() + ", " + pos.getZ() + ")");
-        // the Y we passed earlier will be used here as the minimum spawn height allowed
-    //    if(y >= pos.getY())
-    //    {
-     //       BlockPos bushPos = new BlockPos(pos.getX(), y, pos.getZ());
-            // we know it's on top of grass or dirt, but what is here already?
-     //       Block toReplace = worldIn.getBlockState(bushPos).getBlock();
-            // only place bush if it is air or plant
-     //       if(toReplace == Blocks.AIR )//<--
-                    //|| toReplace == ModBlocks.viniferaPod||toReplace==Blocks.GRASS || toReplace== Blocks.RED_FLOWER||toReplace==Blocks.YELLOW_FLOWER)<--
-      //      {
-                // set the block to a bush
-                // use 2 as the flag to prevent update -- you don't have to include that parameter
-      //          worldIn.setBlockState(bushPos, ModBlocks.viniferaPod.getDefaultState());
-                // debug:
-                // System.out.println("placed a cookie bush!");
-      //      }   // else System.out.println("Sadly, this block is occupied by " + toReplace.getUnlocalizedName());
-       // }
-      //  return false;
-  //  }
-
-    // find a grass or dirt block to place the bush on
-  //  public static int getGroundFromAbove(World world, int x, int z)
-   // {
-     //   int y = 255;
-     //   boolean foundGround = false;
-     //   while(!foundGround && y-- >= 0)
-     //   {
-     //       Block blockAt = world.getBlockState(new BlockPos(x,y,z)).getBlock();
-            // "ground" for our bush is grass or dirt
-     //       foundGround = blockAt == ModBlocks.tiberiumGround;
-     //   }
-
-    //    return y;
-   // }
 }
